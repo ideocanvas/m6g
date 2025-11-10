c49e0ca6ef8534bc66ea00b29dee343c07b94a37
Thomas Tse <thomas_tse@nmt.com.hk>
2025-11-10 15:36:46 +0800

    fix eslint warning

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,8 +1,7 @@
+import { LanguageCode } from '@/lib/i18n';
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { cookies } from 'next/headers';
 import "../globals.css";
-import { LanguageCode } from '@/lib/i18n';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
