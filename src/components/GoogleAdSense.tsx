ffd28d33b91f3ad4f790820ce103dcf344db393e
Thomas Tse <thomas_tse@nmt.com.hk>
2025-11-12 14:31:50 +0800

    update google adsense

--- src/components/GoogleAdSense.tsx
+++ src/components/GoogleAdSense.tsx
@@ -1,20 +1,12 @@
-
 // GoogleAdSense.tsx
 
-import Script from 'next/script';
 
 export const GoogleAdSense = () => {
-// if (process.env.NODE_ENV !== 'production') {
-//   return null;
-// }
-
-return (
-  <Script
-    async
-    src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_YOUR_ADSENSE_ID}`}
-    crossOrigin="anonymous"
-    strategy="afterInteractive"
-  />
-);
+  return (
+    <script
+      async
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_YOUR_ADSENSE_ID}`}
+      crossOrigin="anonymous"
+    />
+  );
 };
-
