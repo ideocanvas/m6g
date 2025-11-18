27e7f4910c2817b2b483aebd9783915589fe0b37
Thomas Tse <thomas_tse@nmt.com.hk>
2025-11-19 01:07:41 +0800

    rollback change

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,32 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-// Create Prisma client based on DATABASE_URL
-function createPrismaClient(useAccelerate?: boolean) {
-  const databaseUrl = process.env.DATABASE_URL;
-  // Auto-detect if accelerate should be used based on DATABASE_URL
-  if (useAccelerate === undefined) {
-    useAccelerate = databaseUrl?.startsWith('prisma://') || databaseUrl?.startsWith('prisma+postgres://');
-  }
-  
-  if (useAccelerate) {
-    console.log('🚀 Using Prisma with Accelerate');
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const { PrismaClient } = require('@prisma/client/edge');
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const { withAccelerate } = require('@prisma/extension-accelerate');
-    return new PrismaClient().$extends(withAccelerate());
-  } else {
-    console.log('📊 Using regular Prisma client (no Accelerate)');
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    const { PrismaClient } = require('@prisma/client');
-    return new PrismaClient();
-  }
-}
-
-// Get DATABASE_URL from environment
-const databaseUrl = process.env.DATABASE_URL
-// Auto-detect if accelerate should be used based on DATABASE_URL
-const useAccelerate = databaseUrl?.startsWith('prisma://') || databaseUrl?.startsWith('prisma+postgres://')
-
-// Create Prisma client with conditional Accelerate extension
-export const prisma = createPrismaClient(useAccelerate) as PrismaClient;
+export const prisma = new PrismaClient().$extends(withAccelerate())
